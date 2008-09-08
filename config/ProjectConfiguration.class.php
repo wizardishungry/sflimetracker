ca1490f72ac651b6a14657110d41d72f6926f1ab
Jon Williams <jonathan.williams@gmail.com>
2008-09-08 15:22:18 -0400

    moved PEAR include_path stuff into ProjectConfiguration

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,12 +1,13 @@
 <?php
-
-//require_once '/opt/local/lib/php/symfony/autoload/sfCoreAutoload.class.php';
-require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
+require_once '/opt/local/lib/php/symfony/autoload/sfCoreAutoload.class.php';
+//require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
 {
   public function setup()
   {
+    // fix for dumb PEAR
+    set_include_path(get_include_path().PATH_SEPARATOR.$this->getRootDir().DIRECTORY_SEPARATOR.'lib');
   }
 }
