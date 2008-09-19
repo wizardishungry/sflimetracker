f04f972a0dcca3123cb0de36997c47210306f045
Jon Williams <jonathan.williams@gmail.com>
2008-09-19 17:37:58 -0400

    beginning of reap code

--- lib/model/ClientPeer.php
+++ lib/model/ClientPeer.php
@@ -9,6 +9,56 @@
  */ 
 class ClientPeer extends BaseClientPeer
 {
+  public static function reap($set)
+  {
+    $items=Array();
+    if(is_array($set))
+    {
+      if(count($set) > 42 ) // fixme magic number
+      {
+        $items=self::prepareReap($set);
+      }
+    }
+    // test if we should do a global reap 
+    $do_global=time()%100==0; // fixme make this sane
+    if($do_global)
+    {
+      $items=array_merge(self::prepareReap($all),$items);
+    }
+    if(count($items)>999 || $do_global) // fixme magic
+    {
+      return self::doDelete($items);
+    }
+    return null;
+  }
+  
+  public static function prepareReap($client)
+  {
+    $result=Array();
+    $counts=Array();
+    foreach($clients as $client)
+    {
+      $id=$client->getTorrentId();
+      if(!isset($result[$id]))
+      {
+        $result[$id]=Array();
+        $counts[$id]=0;
+      }
+      $counts[$id]++;
+      if($client->isCruft())
+      {
+        $result[$id][]=$client->getId();
+      }
+    }
+    $ret=Array();
+    foreach($result as $id=>$items)
+    {
+      if($counts[$id]-count($items)>77) // fixme magic -- making sure there's enough items left over
+        $ret=array_merge($ret,$items);
+    }
+    return $ret;
+  }
+
   public static function retrieveByParameters($params)
   {
     $c = new Criteria();
