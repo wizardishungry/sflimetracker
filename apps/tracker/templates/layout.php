19d09e59446d71620d5f3e90d203f8b0430baf1f
Jon Williams <jonathan.williams@gmail.com>
2008-10-09 15:23:13 -0400

    making the frontpage match wireframes

--- apps/tracker/templates/layout.php
+++ apps/tracker/templates/layout.php
@@ -13,9 +13,18 @@
 <body>
 
 <?php echo $sf_content ?>
+<hr>
 <?php if($sf_user->isAuthenticated()): ?>
   <?php include_partial('account/logoutform') ?>
-<?php else;?>
+<?php else:?>
   <?php echo link_to('Login','account/login') ?>
+<?php endif; ?>
+ |
+<?php
+echo link_to(sfConfig::get('app_version_name'),sfConfig::get('app_version_url'),
+            Array('title'=>sfConfig::get('app_version_comment'))),
+            ' ',sfConfig::get('app_version_rev'),
+            '; Symfony ',SYMFONY_VERSION;
+?>
 </body>
 </html>
