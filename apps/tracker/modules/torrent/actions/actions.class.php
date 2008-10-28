1849a7527e947a25846a2b53c4ace353455f1630
Jon Williams <jonathan.williams@gmail.com>
2008-10-28 17:29:57 -0400

    Redoing form validation for torrent uploads

--- apps/tracker/modules/torrent/actions/actions.class.php
+++ apps/tracker/modules/torrent/actions/actions.class.php
@@ -23,7 +23,7 @@ class torrentActions extends sfActions
     $this->form=new TorrentForm();
     if ($request->isMethod('post'))
     {
-        $this->form->bind($request->getGetParameters(),$request->getFiles());
+        $this->form->bind($request->getPostParameters(),$request->getFiles());
         if ( $this->form->isValid())
         {
             $torrent=new Torrent($this->form->getValue('file'));
@@ -31,7 +31,8 @@ class torrentActions extends sfActions
             $torrent->save();
             $this->redirect('podcast/view?id='.$request->getParameter('podcast_id'));
         } 
-        else throw new sfException('Form failed validation');
+        else
+          return sfView::ERROR;
     }
       $this->form->setDefaults(Array(
             'podcast_id'=>$request->getParameter('podcast_id')
--- lib/form/TorrentForm.class.php
+++ lib/form/TorrentForm.class.php
@@ -12,13 +12,34 @@ class TorrentForm extends BaseTorrentForm
   public function configure()
   {
     $this->setWidgets(Array(
-        'file'=> new sfWidgetFormInputFile(),
         'podcast_id' => new sfWidgetFormInputHidden(),
+        'title'=> new sfWidgetFormInput(),
+        'description' => new sfWidgetFormTextarea(),
+        'web_url'=> new sfWidgetFormInput(),
+        'server_path' => new sfWidgetFormInput(),
+        'file'=> new sfWidgetFormInputFile(),
     ));
 
     $this->setValidators(array(
-        'file' => new sfValidatorFile(),
         'podcast_id' => new sfValidatorInteger(array('required' => false)),
+        'title' => new sfValidatorString(array('required' => false)),
+        'description' => new sfValidatorString(array('required' => false)),
+
+        // three oprions
+        'web_url' => new sfValidatorUrl(Array('required' => false)),
+        'file' => new sfValidatorFile(Array('required' => false)),
+        'server_path' => new sfValidatorString(Array('required' => false)),
+    ));
+
+    $options=Array('required' => true);
+    $messages=Array('required'=>'You must pick at least a file, a url, or a server path');
+
+    $this->validatorSchema->setPostValidator(
+      new sfValidatorOr(array(
+        new sfValidatorSchemaFilter('web_url', new sfValidatorUrl($options,$messages)),
+        new sfValidatorSchemaFilter('file', new sfValidatorFile($options,$messages)),
+        new sfValidatorSchemaFilter('server_path', new sfValidatorString($options,$messages)),
+      )
     ));
     
   }
