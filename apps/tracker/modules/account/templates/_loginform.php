a6f1739f6fc24bbcda14a8ad5ed4cffbb6d2d8cc
Jon Williams <jonathan.williams@gmail.com>
2008-10-29 12:52:38 -0400

    Made login form HTML ready for CSS and jQuery

--- apps/tracker/modules/account/templates/_loginform.php
+++ apps/tracker/modules/account/templates/_loginform.php
@@ -1,9 +1,22 @@
-<form action="<?php echo url_for('account/login') ?>" method="POST">
-  <?php
-    if(!isset($form))
-      $form=new LoginForm();
-    echo $form;
-  ?>
-  <input type="submit" value="login" />
-</form>
-
+<div class="login">
+  <div class="heading">
+    <h1><?php echo sfConfig::get('app_version_name') ?></h1>
+    <h4 title="<?php echo sfConfig::get('app_version_comment') ?>">
+      version <?php echo sfConfig::get('app_version_rev') ?>
+    </h4>
+    <p>Please enter your password to continue</p>
+  </div>
+  <form action="<?php echo url_for('account/login') ?>" method="POST">
+    <table>
+      <?php
+        if(!isset($form))
+          $form=new LoginForm();
+        echo $form;
+      ?>
+      <tr>
+        <td><input type="submit" value="login" /></td>
+        <td><?php echo button_to_function('I forgot my password!', "alert('Replace this with a jQuery box that tells you how to reset pw todo!')") ?></td>
+      </tr>
+    </table>
+  </form>
+</div>
--- apps/tracker/modules/account/templates/loginError.php
+++ apps/tracker/modules/account/templates/loginError.php
@@ -1,2 +1,2 @@
-Oops, you probably entered your password wrong!
+<span class="error">Oops, you probably entered your password wrong!</span>
 <?php include_partial('account/loginform') ?>
