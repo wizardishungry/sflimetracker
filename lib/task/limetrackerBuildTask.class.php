0cb3aa0be588b20d6a90fde96a5292b68d991c52
Jon Williams <jonathan.williams@gmail.com>
2008-11-06 15:32:47 -0500

    Changed arguments of build script and added more checking

--- lib/task/limetrackerBuildTask.class.php
+++ lib/task/limetrackerBuildTask.class.php
@@ -10,23 +10,33 @@ class limetrackerBuildTask extends sfBaseTask
     $this->detailedDescription = <<<EOF
 The [limetracker:build|INFO] task does things to setup a complete build of LimeTracker.
 EOF;
-    $this->addArgument('file', sfCommandArgument::REQUIRED, 'The path to the zipfile',null);
     $this->addArgument('version', sfCommandArgument::OPTIONAL, 'The application version',null);
+    $this->addArgument('file', sfCommandArgument::OPTIONAL, 'The path to the zipfile',null);
     $this->addOption('verbose', null, sfCommandOption::PARAMETER_REQUIRED, 'Enables verbose output', false);
     
   }
 
   protected function execute($arguments = array(), $options = array())
   {
-    if(!$this->checkPreconditions()) return;
+    if(!$this->checkPreconditions($arguments,$options)) return;
 
     $tasks = Array(
-      Array(new limetrackerMetaTask($this->dispatcher, $this->formatter), Array('version'=>@$arguments['version']) , Array('verbose'=>@$options['verbose']) ),
-      Array(new sfPropelBuildModelTask($this->dispatcher, $this->formatter), Array() , Array('verbose'=>@$options['verbose']) ),
-      Array(new sfPropelBuildSqlTask($this->dispatcher, $this->formatter), Array() , Array('verbose'=>@$options['verbose']) ),
-      Array(new sfPropelBuildFormsTask($this->dispatcher, $this->formatter), Array() , Array('verbose'=>@$options['verbose']) ),
-      Array(new limetrackerZipTask($this->dispatcher, $this->formatter), Array('file'=>$arguments['file']) , Array('verbose'=>@$options['verbose']) ),
+      Array(new limetrackerMetaTask($this->dispatcher, $this->formatter),
+        Array('version'=>@$arguments['version']) , Array('verbose'=>@$options['verbose']) ),
+      Array(new sfPropelBuildModelTask($this->dispatcher, $this->formatter),
+        Array() , Array('verbose'=>@$options['verbose']) ),
+      Array(new sfPropelBuildSqlTask($this->dispatcher, $this->formatter),
+        Array() , Array('verbose'=>@$options['verbose']) ),
+      Array(new sfPropelBuildFormsTask($this->dispatcher, $this->formatter),
+        Array() , Array('verbose'=>@$options['verbose']) ),
     );
+    if(@$arguments['file'])
+    {
+      $tasks[]=Array(new limetrackerZipTask($this->dispatcher, $this->formatter),
+        Array('file'=>$arguments['file']) , Array('verbose'=>@$options['verbose']) );
+      $tasks[]=Array(new limetrackerMetaTask($this->dispatcher, $this->formatter),
+        Array('version'=>null), Array('verbose'=>@$options['verbose']) );
+    }
     foreach($tasks as $list)
     {
       list($task,$args,$opts)=$list;
@@ -35,12 +45,24 @@ EOF;
     }
   }
 
-  protected function checkPreconditions()
+  protected function checkPreconditions($arguments,$options)
   {
     $errors=Array();
 
-    if(!class_exists('ZipArchive')) $errors[]='Zip support needs to be compiled into PHP';
-    if(`git`==null) $errors[]='You need an install of git in your $PATH';
+    if(  (!$arguments['file'] || !$arguments['version']) && ($arguments['file'] || $arguments['version']  ) )
+      $errors[]='You must provide both an filename and version or neither.';
+
+    if($arguments['file']&&!class_exists('ZipArchive'))
+      $errors[]='Zip support needs to be compiled into PHP.';
+
+    if(!$arguments['version']&&`git`==null)
+      $errors[]='You need an install of git in your $PATH.';
+
+    if($arguments['version']&&(!preg_match('#^\d+\.\d\d#',$arguments['version']) ))
+      $errors[]='Version must be of the form 1.00';
+
+    if($arguments['file']&&!preg_match('#\.zip#',$arguments['file']))
+      $errors[]='File name must end in zip.';
 
     if(count($errors))
     {
