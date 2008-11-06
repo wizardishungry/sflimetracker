31aa176128c0e7af2ae03a07bd576ef3030245be
Jon Williams <jonathan.williams@gmail.com>
2008-11-06 14:11:34 -0500

    Added PHP version check.

--- web/index.php
+++ web/index.php
@@ -1,5 +1,9 @@
 <?php
 
+if (version_compare(PHP_VERSION, '5.1.0') === -1) {
+    exit('PHP version 5.1.0+ required; ' . PHP_VERSION . " installed\n");
+}
+
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('tracker', 'prod', true);
--- web/tracker_dev.php
+++ web/tracker_dev.php
@@ -1,4 +1,8 @@
 <?php
+if (version_compare(PHP_VERSION, '5.1.0') === -1) {
+    exit('PHP version 5.1.0+ required; ' . PHP_VERSION . " installed\n");
+}
+
 // this check prevents access to debug front conrollers that are deployed by accident to production servers.
 // feel free to remove this, extend it or make something more sophisticated.
 /*if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1')))
