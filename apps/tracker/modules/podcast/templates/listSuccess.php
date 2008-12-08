d2ef71278b6a88bb0b13a841799824b00fa90fdc
Michael Nutt <michael@nuttnet.net>
2008-12-08 15:27:16 -0500

    Added icons for 'web' and 'add'

--- apps/tracker/modules/podcast/templates/listSuccess.php
+++ apps/tracker/modules/podcast/templates/listSuccess.php
@@ -1,4 +1,4 @@
-<h3>Podcasts</h3>
+<h2>Podcasts</h2>
 <ul>
   <?php foreach($podcasts as $podcast): ?>
     <li>
@@ -6,12 +6,14 @@
         link_to($podcast->getTitle(),'podcast/view?id='.$podcast->getId());
       ?>
       <ul>
-        <li>[web] <?php 
-          $url=url_for('podcast/view?id='.$podcast->getId(),true);
-          echo link_to($url,$url);
-        ?></li>
-        <li>[add] <?php 
-          echo link_to('Add…','episode/add?podcast_id='.$podcast->getId());
+        <li>
+	  <?php $url= url_for('podcast/view?id='.$podcast->getId(),true); ?>
+	  <a href="<?php echo $url ?>">
+            <img class="inline_icon" src="/images/icons/web.png" /><?php echo $url ?>
+	  </a>
+	</li>
+        <li><?php 
+          echo link_to('<img class="inline_icon" src="/images/icons/add.png" />Add…','episode/add?podcast_id='.$podcast->getId());
         ?></li>
       </ul>
     </li>
@@ -19,7 +21,7 @@
   <li><?php echo link_to('New Podcast…','podcast/add') ?></li>
 </ul>
 
-<h3>Files</h3>
+<h2>Files</h2>
 <ul>
   <li><?php echo link_to('Add File…','podcast/add') ?>
 </li>
