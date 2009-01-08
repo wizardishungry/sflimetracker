412a47167c60158f5e63d12d85b3c14d6efa65aa
Jon Williams <jonathan.williams@gmail.com>
2009-01-08 11:20:28 -0500

    reorganizing common orm behavior

--- lib/CommonBehavior.class.php
+++ lib/CommonBehavior.class.php
@@ -1,6 +1,10 @@
 <?php
     /*
         Common behaviors to keep stuff shorthand
+        This isn't actually a propel behavior per-say because it was refactored
+        to get around the stupid way static fields/methods are handled.
+         -- "Unfortunately, as of PHP 5, static method calls cannot be caught
+        by a __call()."
     */
 class CommonBehavior
 {
