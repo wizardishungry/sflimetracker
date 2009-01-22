1a404686286355e5b816cd80191dad69fd44cc46
Jon Williams <jonathan.williams@gmail.com>
2009-01-22 17:19:25 -0500

    refactoring to use disconnectedCurl

--- lib/sfValidatedFileFromUrl.class.php
+++ lib/sfValidatedFileFromUrl.class.php
@@ -11,25 +11,28 @@ class sfValidatedFileFromUrl extends sfValidatedFile
   {
     register_shutdown_function(array(&$this, "__destruct"));
 
+    /*
     $b=new sfWebBrowser(array(
         'user_agent' => sfConfig::get('app_version_name').' ('. sfConfig::get('app_version_rev'). ' '
           .sfConfig::get('app_version_comment'). ') '. sfConfig::get('app_version_url') , 
         'timeout'    => 15
     )); // refactor this into our own browser wrapper class todo
+    */
 
-    $this->tempName=tempnam(sys_get_temp_dir(),sfConfig::get('app_version_name')); // is there a symfony way of getting tmp dir
+
+
     $original_url=$url;
-    $original_name=preg_replace('#^.*/#','',$original_url);
+    $original_name=preg_replace('#^.*/#','',$original_url); // we'll need to do this for redirects fixme
 
+    $b = new disconnectedCurl($url);
     // probably want to have a way to gather statistics here
-    $b->get($url); // we should HEAD this first -- giant files will not work well with this method
+    $b=run();
+    // we should HEAD this first -- giant files will not work well with this method
 
-    $mime_type=$b->getResponseHeader('Content-Type'); // of course we shouldn't trust people to have their servers configured right…sigh todo
-    $mime_type=preg_replace('/;.*$/','',$mime_type); // remove stuff after the semicolon delimiter "charset=UTF-8" etc
+    //$mime_type=$b->getResponseHeader('Content-Type'); // of course we shouldn't trust people to have their servers configured right…sigh todo
+    //$mime_type=preg_replace('/;.*$/','',$mime_type); // remove stuff after the semicolon delimiter "charset=UTF-8" etc
 
-    if(!file_put_contents($this->tempName,$b->getResponseText(),LOCK_EX)) // as above, we don't wanna to do strings with huge files
-      throw new sfException("Couldn't write out temp file");
-    parent::__construct($original_name,$mime_type,$this->tempName,filesize($this->tempName));
+    parent::__construct($original_name,$mime_type,$b->getName(),filesize($b->getName()));
   }
 
   function __destruct()
