c005d11716d3e84cff64337bf0765263b5f3609f
Jon Williams <jonathan.williams@gmail.com>
2009-01-23 14:23:11 -0500

    make sfValidatedFileFromUrl use disconnectedCurl

--- lib/sfValidatedFileFromUrl.class.php
+++ lib/sfValidatedFileFromUrl.class.php
@@ -11,28 +11,26 @@ class sfValidatedFileFromUrl extends sfValidatedFile
   {
     register_shutdown_function(array(&$this, "__destruct"));
 
-    /*
-    $b=new sfWebBrowser(array(
-        'user_agent' => sfConfig::get('app_version_name').' ('. sfConfig::get('app_version_rev'). ' '
+    $options=Array(
+        CURLOPT_USERAGENT => sfConfig::get('app_version_name').' ('. sfConfig::get('app_version_rev'). ' '
           .sfConfig::get('app_version_comment'). ') '. sfConfig::get('app_version_url') , 
-        'timeout'    => 15
-    )); // refactor this into our own browser wrapper class todo
-    */
-
+    );
 
 
     $original_url=$url;
     $original_name=preg_replace('#^.*/#','',$original_url); // we'll need to do this for redirects fixme
 
-    $b = new disconnectedCurl($url);
+    $b = new disconnectedCurl($url,$options);
     // probably want to have a way to gather statistics here
-    $b=run();
-    // we should HEAD this first -- giant files will not work well with this method
+    $b->run(null);
+    // we should HEAD this first
+    
+    if($b->isRunning()) throw new sfException('still running');
 
     //$mime_type=$b->getResponseHeader('Content-Type'); // of course we shouldn't trust people to have their servers configured right…sigh todo
     //$mime_type=preg_replace('/;.*$/','',$mime_type); // remove stuff after the semicolon delimiter "charset=UTF-8" etc
 
-    parent::__construct($original_name,$mime_type,$b->getName(),filesize($b->getName()));
+    parent::__construct($original_name,$mime_type,$b->getFile(),filesize($b->getFile()));
   }
 
   function __destruct()
