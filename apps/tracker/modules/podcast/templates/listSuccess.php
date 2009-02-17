37496f319ec05c0a2888ecae95ea3551d3e795a6
Jon Williams <jonathan.williams@gmail.com>
2009-02-17 16:00:07 -0500

    Change homepage to match spec.

--- apps/tracker/modules/podcast/templates/listSuccess.php
+++ apps/tracker/modules/podcast/templates/listSuccess.php
@@ -2,17 +2,13 @@
   <div class="podcasts">
   <?php foreach($podcasts as $podcast): ?>
     <h3>
-        <?php echo link_to_with_icon($podcast->getTitle(), "web", $podcast->getUri()); ?>
+        <?php echo link_to_with_icon($podcast->getTitle(), "cog", 'podcast/edit?id='.$podcast->getId()); ?>
     </h3>
     <ul>
         <li>
-            <div> &nbsp; 
-            <?php echo link_to_with_icon('Edit', "cog", 'podcast/edit?id='.$podcast->getId()); ?>
+            &nbsp;
             <?php echo link_to_with_icon('Add episode', "add", 'episode/add?podcast_id='.$podcast->getId()); ?>
-            </div>
         </li>
     </ul>
   <?php endforeach; ?>
-  <?php if($sf_user->isAuthenticated()): ?>
-    <p><?php echo link_to_with_icon('New podcast…', "add", 'podcast/add'); ?></p>
-  <?php endif; ?>
+<p><?php echo link_to_with_icon('New podcast…', "add", 'podcast/add'); ?></p>
