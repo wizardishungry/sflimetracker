49fc766806461acfd894d41035c81c45b25ccc9e
Jon Williams <jonathan.williams@gmail.com>
2009-02-26 14:10:48 -0500

    have the controllers find the configuration correctly

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ if (version_compare(PHP_VERSION, '5.1.0') === -1) {
     exit('PHP version 5.1.0+ required; ' . PHP_VERSION . " installed\n");
 }
 
-require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
+require_once(dirname(__FILE__).'/config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('tracker', 'prod', true);
 sfContext::createInstance($configuration)->dispatch();
