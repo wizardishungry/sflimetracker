bac86473e02df5a8c3100ade1a3b2e41ad54b2dc
Jon Williams <jonathan.williams@gmail.com>
2009-03-06 15:17:10 -0500

    prepare unit test for simplified password approach

--- test/unit/trackerUserTest.php
+++ test/unit/trackerUserTest.php
@@ -7,13 +7,6 @@ $name = sfConfig::get('app_version_name');
 $t = new lime_test(5, new lime_output_color());
 $t->isa_ok($user=new trackerUser(new sfEventDispatcher(), new sfSessionTestStorage()),'trackerUser','new trackerUser()');
 $t->isa_ok($user->getCryptedString(),'string','getCryptedString()');
-if(file_get_contents("$sf_root_dir/.htpasswd")!=file_get_contents("$sf_root_dir/.htpasswd.dist"))
-{
-  $t->skip("Skipping tests -- cp .htpasswd.dist .htpasswd to have them run",3);
-}
-else
-{
-  $t->is($user->getCryptedString(),trackerUser::crypt($name),"getCryptedString() == sha1('$name')");
-  $t->ok($user->authenticatePassword($name),"authenticatePassword() works with correct password");
-  $t->ok(!$user->authenticatePassword($name.'hurble durble'),"authenticatePassword() fails with incorrect password");
-}
+$t->is($user->getCryptedString(),trackerUser::crypt($name),"getCryptedString() == sha1('$name')");
+$t->ok($user->authenticatePassword($name),"authenticatePassword() works with correct password");
+$t->ok(!$user->authenticatePassword($name.'hurble durble'),"authenticatePassword() fails with incorrect password");
