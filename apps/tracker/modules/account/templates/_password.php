fdb8ffa472dc026b2d3175c6659f04ef75ebeb89
Jon Williams <jonathan.williams@gmail.com>
2009-03-10 13:30:11 -0400

    password as template

--- apps/tracker/modules/account/templates/_password.php
+++ apps/tracker/modules/account/templates/_password.php
@@ -1,8 +1,27 @@
 <form action="<?php echo url_for('account/password') ?>" method="POST">
+<?php echo $form['_csrf_token'] ?>
   <table>
-    <?php
-      echo $form;
-    ?>
+    <tr>
+        <th><?php echo $form['current_password']->renderLabel() ?>:</th>
+        <td>
+        <?php echo $form['current_password']->renderError() ?>
+        <?php echo $form['current_password'] ?>
+        </td>
+    </tr>
+    <tr>
+        <th><?php echo $form['password']->renderLabel() ?>:</th>
+        <td>
+        <?php echo $form['password']->renderError() ?>
+        <?php echo $form['password'] ?>
+        </td>
+    </tr>
+    <tr>
+        <th><?php echo $form['password_again']->renderLabel() ?>:</th>
+        <td>
+        <?php echo $form['password_again']->renderError() ?>
+        <?php echo $form['password_again'] ?>
+        </td>
+    </tr>
     <tr>
       <td>&nbsp;</td>
       <td><input type="submit" value="Change password" />
