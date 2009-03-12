1a55519adafcb114fb6e336aa2236a75c9a61b1e
Jon Williams <jonathan.williams@gmail.com>
2009-03-12 17:27:38 -0400

    make sure message about php versions less than 5 is darn well displayed

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
 if (version_compare(PHP_VERSION, '5.1.0') === -1) {
-    exit('PHP version 5.1.0+ required; ' . PHP_VERSION . " installed\n");
+    $message='PHP version 5.1.0+ required; ' . PHP_VERSION . " installed\n";
+    echo "<h3>$message</h3>";
+    exit($message);
 }
 
 require_once(dirname(__FILE__).'/config/ProjectConfiguration.class.php');
--- tracker_dev.php
+++ tracker_dev.php
@@ -1,6 +1,8 @@
 <?php
 if (version_compare(PHP_VERSION, '5.1.0') === -1) {
-    exit('PHP version 5.1.0+ required; ' . PHP_VERSION . " installed\n");
+    $message='PHP version 5.1.0+ required; ' . PHP_VERSION . " installed\n";
+    echo "<h3>$message</h3>";
+    exit($message);
 }
 
 // this check prevents access to debug front conrollers that are deployed by accident to production servers.
