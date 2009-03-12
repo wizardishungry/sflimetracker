d58d83622da4872c1fea83166a0691ac61c6f5f7
Jon Williams <jonathan.williams@gmail.com>
2009-03-12 15:43:48 -0400

    Resolves CAST-104 via a dl() of PDO extension https://www.limewire.org/jira/browse/CAST-104

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -5,15 +5,38 @@ sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
 {
+
   public function setup()
   {
+
+    if(!class_exists('PDO',false))
+    {
+        $this->loadSharedObjects(Array('pdo','pdo_sqlite','sqlite')); // todo support mysql
+    }
+
       $this->enableAllPluginsExcept(array('sfDoctrinePlugin'));
   // fix for dumb PEAR
     set_include_path(get_include_path().PATH_SEPARATOR.$this->getRootDir().DIRECTORY_SEPARATOR.'lib');
   }
+
   public function setRootDir($rootDir)
   {
     parent::setRootDir($rootDir);
     $this->setWebDir($rootDir);
+  }
+
+  public function loadSharedObjects($objects)
+  {
+    if(!function_exists('dl'))
+        throw new sfException("Couldn't dl(); dl not defined");
+
+    if(!ini_get('enable_dl'))
+        throw new sfException("Couldn't dl(); ini_get reports enable_dl disabled in php.ini");
+
+    foreach($objects as $o)
+    {
+        if(!dl($o.'.so'))
+            throw new sfException("Couldn't dl(\"$o.so\")");
+    }
   }
 }
