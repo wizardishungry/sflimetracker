204af12115187e2181f8b6b1c54fd5f7ceddcd31
Jon Williams <jonathan.williams@gmail.com>
2009-03-24 14:11:18 -0400

    only 48k more for all the sf debugging stuff

--- lib/task/limetrackerZipTask.class.php
+++ lib/task/limetrackerZipTask.class.php
@@ -154,7 +154,6 @@ EOF;
       'plugins/.*',
       'plugins/File_Bittorrent2', // added via symlinks in lib
       'test',       // tests don't bleong in an end user release
-      'sf/sf_*', // no debugging
       'tracker_dev.php*', // no debugging
       '*.swp', '*/*.swp', // no vim swapfiles
     );
