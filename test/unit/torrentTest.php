cb4ff88092126bb1fbe1c31bdd30f1c0dd8d1440
Jon Williams <jonathan.williams@gmail.com>
2009-04-01 17:33:22 -0400

    fixed torrentTest

--- test/unit/torrentTest.php
+++ test/unit/torrentTest.php
@@ -8,16 +8,19 @@ class testValidatedFile extends sfValidatedFile
   function __construct($originalName, $type, $tempName, $size){}
   public function getOriginalName() { return preg_replace('#\.[^.]*$#','',basename(__FILE__)); }
   public function getOriginalExtension($default='') { return preg_replace('#^.*\.#','',__FILE__); }
-  public function save($file, $fileMode = 0666, $create = true, $dirMode = 0777) { return copy(__FILE__,$this->saved_name=$file); }
+  public function save($file = null, $fileMode = 0666, $create = true, $dirMode = 0777) { return copy(__FILE__,$this->saved_name=$file); }
   public function getSavedName() { return $this->saved_name; }
+  public function getTempName() { return __FILE__; /*better not call unlink() :) */ }
 }
 
 $file=new testValidatedFile(1,2,3,4);
 sfConfig::set('sf_upload_dir',sys_get_temp_dir());
 
-$t = new lime_test(4, new lime_output_color());
+$t = new lime_test(3, new lime_output_color());
 
-$t->isa_ok($p=new Torrent($file),'Torrent','constructor works');
+$p=new Torrent($file);
+$p->setFile($file);
+
 $t->ok(file_exists($p->getTorrentPath()), '.torrent exists at path'.$p->getTorrentPath());
 $t->isa_ok($p->getFeedEnclosure(),'sfFeedEnclosure','got feed enclosure');
 
