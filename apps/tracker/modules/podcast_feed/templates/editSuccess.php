2e3314ff82b581511b501a7f4e134f6bb48e43b8
Michael Nutt <michael@nuttnet.net>
2009-04-09 15:41:45 -0400

    form wrapper for podcast_feed

--- apps/tracker/modules/podcast_feed/templates/editSuccess.php
+++ apps/tracker/modules/podcast_feed/templates/editSuccess.php
@@ -1,7 +1,8 @@
 <h1>Editing format for podcast <?php echo link_to($podcast->getTitle(),'podcast/edit?id='.$podcast->getId()) ?></h1>
 
-<form action="<?php echo url_for('podcast_feed/edit') ?>" method="POST" enctype="multipart/form-data">
-<table>
+<div class="form-wrapper">
+  <form action="<?php echo url_for('podcast_feed/edit') ?>" method="POST" enctype="multipart/form-data">
+  <table>
     <?php echo $form ?>
     <tr>
     <td>&nbsp;</td>
@@ -12,6 +13,8 @@
 
     </td>
     </tr>
-</table>
-</form>
+  </table>
+  </form>
+</div>
+
 <?php echo delete_form_for_object($podcast_feed,'podcast_feed/delete'); ?>
