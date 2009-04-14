bedfb0184f032716dbaffe4add6fea5ed211ab3e
Michael Nutt <michael@nuttnet.net>
2009-04-14 19:27:12 -0400

    Make format form work the same as other forms

--- apps/tracker/modules/podcast_feed/templates/editSuccess.php
+++ apps/tracker/modules/podcast_feed/templates/editSuccess.php
@@ -1,9 +1,39 @@
 <h1>Editing format for podcast <?php echo link_to($podcast->getTitle(),'podcast/edit?id='.$podcast->getId()) ?></h1>
 
-<div class="form-wrapper">
+<div class="form-wrapper open-form">
   <form action="<?php echo url_for('podcast_feed/edit') ?>" method="POST" enctype="multipart/form-data">
   <table>
-    <?php echo $form ?>
+<tr>
+	<th><?php echo $form['title']->renderLabel() ?></th>
+	<td>
+		<div class="form-field">
+			<?php echo $form['title'] ?>
+			<p>Becomes <span class="perm-preview">(empty)</span> in torrent filenames</p>
+			<?php echo $form['title']->renderError() ?>
+		</div>
+		<script type="text/javascript">
+		  watchForSlug($$('input#title').first(), $$('.perm-preview').first());
+		</script>
+		<div class="value">
+			<?php echo $form['title']->getValue(); ?>
+			<a href="#" class="edit-button">edit</a>
+		</div>
+	</td>
+</tr>
+<tr>
+	<th><?php echo $form['rss_url']->renderLabel() ?></th>
+	<td>
+		<div class="form-field">
+			<?php echo $form['rss_url'] ?>
+			<p>Url for the format's feed</p>
+			<?php echo $form['rss_url']->renderError() ?>
+		</div>
+		<div class="value">
+			<?php echo $form['rss_url']->getValue(); ?>
+			<a href="#" class="edit-button">edit</a>
+		</div>
+	</td>
+</tr>
     <tr>
     <td>&nbsp;</td>
     <td>
