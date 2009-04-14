345a821eaca8ab0d48ba63912031c240b54de46b
Michael Nutt <michael@nuttnet.net>
2009-04-14 15:30:05 -0400

    Open form on add podcast

--- apps/tracker/modules/podcast/templates/addSuccess.php
+++ apps/tracker/modules/podcast/templates/addSuccess.php
@@ -1,5 +1,5 @@
 <h1>Add Podcast</h1>
-<div class="form-wrapper add">
+<div class="form-wrapper open-form">
   <form action="<?php echo url_for('podcast/add') ?>" method="POST" enctype="multipart/form-data">
     <table>
       <?php include_partial('podcast/podcastform', Array('form'=>$form)); ?>
