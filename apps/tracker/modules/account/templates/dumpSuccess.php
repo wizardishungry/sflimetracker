64a09e7031337e4903c6cb1788f40555dbcc184b
Jon Williams <jonathan.williams@gmail.com>
2009-04-14 17:13:40 -0400

    add linebreak to yaml

--- apps/tracker/modules/account/templates/dumpSuccess.php
+++ apps/tracker/modules/account/templates/dumpSuccess.php
@@ -1,4 +1,5 @@
 # As a convenience to you, a copy of this data has been placed on the server at <?php echo $path ?>
+
 #<?php echo sfConfig::get('app_version_name'), ' ', sfConfig::get('app_version_rev');?>
 
 <?php
