855f19d61f368f0964890dcf58fa3d0633ce9fd4
Michael Nutt <michael@nuttnet.net>
2009-04-16 13:34:15 -0400

    Fix typo in template

--- apps/tracker/modules/episode/templates/editSuccess.php
+++ apps/tracker/modules/episode/templates/editSuccess.php
@@ -32,7 +32,7 @@
   <?php endforeach; ?>
 </ul>
 
-<h3>Add new format</h4>
+<h3>Add new format</h3>
 <div class="form-wrapper">
   <form action="<?php echo url_for('podcast_feed/add') ?>" method="POST" enctype="multipart/form-data">
   <table>
