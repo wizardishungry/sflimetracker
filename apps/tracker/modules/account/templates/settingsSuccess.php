4456ed42c96e100b98716227a4ce00307dad7230
Jon Williams <jonathan.williams@gmail.com>
2009-04-20 14:47:51 -0400

    nice looking settings page

--- apps/tracker/modules/account/templates/settingsSuccess.php
+++ apps/tracker/modules/account/templates/settingsSuccess.php
@@ -1,23 +1,65 @@
-<h1>Tracker Settings</h1>
+<h1>Settings</h1>
+
+<h3>Database</h3>
+
+<?php
+    $db = sfContext::getInstance()->getDatabaseManager()->getDatabase('propel'); // symfony uses "default" by default!
+    $db_params=$db->getParameterHolder()->getAll(); 
+
+    $dsn=$db_params['dsn'];
+?>
 
 <p>
-    More settings
+    <blockquote>
+        <code><?php echo $dsn; ?></code>
+    </blockquote>
 </p>
 
-<h3>Database Connectivity</h3>
 
-<?php include_partial('databaseinfo') ?>
 <ul>
     <li>
     <form  action='<?php echo url_for('account/dump') ?>' method="POST">
         <?php echo new sfForm(); ?>
-        <input type='submit' value='Dump'>
+        <input type='submit' value='Export Data'>
     </form>
     </li>
     <li>
-    <form  action='<?php echo url_for('account/restore') ?>' method="POST" enctype="multipart/form-data">
+    <form action='<?php echo url_for('account/restore') ?>' method="POST" enctype="multipart/form-data">
         <?php echo new RestoreForm(); ?>
-        <input type='submit' value='Restore'>
+        <input type='submit' value='Import Data'>
     </form>
+    </li>
+</ul>
+
+<h3>Setup</h3>
+
+<ul>
+    <li><?php echo link_to('Change password','account/password') ?></li>
+    <li><?php echo link_to('Delete everything and start over','account/blam') ?></li>
+</ul>
+
+<h3>Diagnostics</h3>
+<?php $url_prefix = 'http://'. $sf_request->getHost().$sf_request->getRelativeUrlRoot().'/'; ?>
+<ul>
+    <li>
+        Database
+        <?php 
+            $url=url_for('account/dbinfo',true);
+            echo link_to($url,$url);
+        ?>
+    </li>
+    <li>
+        Environment
+        <?php
+            $url=$url_prefix.'limetracker-assert.php';
+            echo link_to($url,$url);
+        ?>
+    </li>
+    <li>
+        PHPinfo
+        <?php
+            $url.='?phpinfo=sure';
+            echo link_to($url,$url);
+        ?>
     </li>
 </ul>
