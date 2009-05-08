77b4084888e6944b7e73b82e261d712f65f6687f
Jon Williams <jonathan.williams@gmail.com>
2009-05-08 13:57:49 -0400

    exception handling

--- config/error/error.html.php
+++ config/error/error.html.php
@@ -1,1 +1,6 @@
+<?php if($error instanceof limeException): ?>
 <?php echo $exception; ?>
+<?php else: ?>
+This exception does not appear to have an associated help page. Bummer.
+<?php echo $exception; ?>
+<?php endif; ?> 
