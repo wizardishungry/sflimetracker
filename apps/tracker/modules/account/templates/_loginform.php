5a9cf022c8e1005a0aa78467c29f8812ed4212af
Jon Williams <jonathan.williams@gmail.com>
2009-05-27 15:02:36 -0400

    Change and forget links go above Remeber me, not after

--- apps/tracker/modules/account/templates/_loginform.php
+++ apps/tracker/modules/account/templates/_loginform.php
@@ -5,18 +5,18 @@
     <?php echo $form['password']->renderLabel() ?>
     <?php echo $form['password'] ?>
   </div>
+  
+  <div class="links">
+    <?php echo link_to('Change',"account/password"); ?>
+    &middot;
+    <?php echo link_to('I forgot',"http://limecast.com/tracker/reset-password"); ?>
+  </div>
 
   <div id="remember_field">
     <?php echo $form['remember_me']->renderError() ?>
     <?php echo $form['remember_me'] ?>
     <?php echo $form['remember_me']->renderLabel(null, array('id' => 'remember_label')) ?>
   </div>
-
-  <div class="links">
-    <?php echo link_to('Change',"account/password"); ?>
-    &middot;
-    <?php echo link_to('I forgot',"http://limecast.com/tracker/reset-password"); ?>
-</div>
 
   <input type="submit" class="submit" value="Sign in" />
 </form>
